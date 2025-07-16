6bd0d5aebe9b8d95a85003d43882c7b2e50eec61
mkae21 <bbuchu123@gmail.com>
2025-07-16 23:27:39 +0900

    Update

--- Uni-Run/Assets/Scripts/BackgroundLoop.cs
+++ Uni-Run/Assets/Scripts/BackgroundLoop.cs
@@ -6,14 +6,26 @@ public class BackgroundLoop : MonoBehaviour {
 
     private void Awake() {
         // 가로 길이를 측정하는 처리
+        boxCollider2D = GetComponent<boxCollider2D>();
+        if(boxCollider2D != null)
+        {
+            width = boxCollider2D.size.x;
+        }
     }
 
     private void Update() {
         // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을때 위치를 리셋
+        if(transform.position.x <= -width)
+        {
+            Reposition();
+        }
     }
 
     // 위치를 리셋하는 메서드
     private void Reposition() {
-        
+        // transform.position.x = width * 2;
+
+        Vector2 offset = new Vector2(width * 2, 0);
+        transform.position = (Vector2)transform.position + offset;
     }
 }
